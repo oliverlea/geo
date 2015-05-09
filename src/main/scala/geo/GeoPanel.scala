e44e391491c39b05aef964405274921b7f82b7f6
crembo <cremboc@gmail.com>
2015-05-09 17:26:51 +0100

    Moveable rectable with keyboard

--- src/main/scala/geo/GeoPanel.scala
+++ src/main/scala/geo/GeoPanel.scala
@@ -2,7 +2,7 @@ package geo
 
 import java.awt.event._
 import java.awt.{Graphics, Point}
-import javax.swing.JPanel
+import javax.swing.{AbstractAction, JPanel, KeyStroke}
 
 import geo.domain.GPoint
 
@@ -23,32 +23,36 @@ class GeoPanel extends JPanel {
 		}
 	})
 
-	addKeyListener(new KeyListener {
+	val im = getInputMap
+	im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), KeyEvent.VK_W)
+	im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), KeyEvent.VK_A)
+	im.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), KeyEvent.VK_S)
+	im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), KeyEvent.VK_D)
 
-		override def keyTyped(e: KeyEvent): Unit = {
-			val key = e.getKeyCode
+	val am = getActionMap
 
-			key match {
-				case KeyEvent.VK_W =>
-					position += new Point(0, 5)
-				case KeyEvent.VK_A =>
-					position += new Point(-5, 0)
-				case KeyEvent.VK_S =>
-					position += new Point(0, -5)
-				case KeyEvent.VK_D =>
-					position += new Point(5, 0)
-				case _ =>
-			}
-
+	am.put(KeyEvent.VK_W, new AbstractAction() {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			position += new Point(0, -5)
 			repaint()
 		}
-
-		override def keyPressed(e: KeyEvent): Unit = {
-
+	})
+	am.put(KeyEvent.VK_A, new AbstractAction() {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			position += new Point(-5, 0)
+			repaint()
 		}
-
-		override def keyReleased(e: KeyEvent): Unit = {
-
+	})
+	am.put(KeyEvent.VK_S, new AbstractAction() {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			position += new Point(0, 5)
+			repaint()
+		}
+	})
+	am.put(KeyEvent.VK_D, new AbstractAction() {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			position += new Point(5, 0)
+			repaint()
 		}
 	})
 
